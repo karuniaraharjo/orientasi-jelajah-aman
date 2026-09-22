2fce91330674f7abfc0bf3b7db31750eaf8b266e
karuniaraharjo <karuniaraharjo06@gmail.com>
2026-09-22 09:32:35 +0700

    feat: tambah modal tambah favorit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from "expo-router";
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="detail/[kota]" options={{ title: "Detail Cuaca" }} />
+      <Stack.Screen
+        name="tambah-favorit"
+        options={{ presentation: "modal", title: "Tambah Favorit" }}
+      />
+    </Stack>
+  );
+}
